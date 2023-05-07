0b747a7fe8e69b427473d6bb9472fc598fe5a6e7
Nikita Pravoi <nikitapravoi@yandex.ru>
2023-05-07 21:40:51 +0700

    Updated models.py, views.py

--- djangoNews/settings.py
+++ djangoNews/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'djangoNews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'news/../templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -117,8 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'news/static')
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = 'media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- news/views.py
+++ news/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+
+def get_html(request):
+    return render(request, 'news/card.html')
