17135da2e01603443b5f0ac8e15e23d6de98420f
Nikita Pravoi <nikitapravoi@yandex.ru>
2023-05-17 18:03:23 +0700

    Created models News, Work, Project in models.py and registered them in admin.py

--- news/models.py
+++ news/models.py
@@ -3,9 +3,8 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
-
-class User(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+# class User(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
 class PublishedManager(models.Manager):
@@ -35,6 +34,42 @@ class Event(models.Model):
         indexes = [
             models.Index(fields=['-publish']),
         ]
+
+    def __str__(self):
+        return self.title
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Work(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    participants = models.ManyToManyField(User, related_name='works_participated')
+    author = models.ForeignKey(User, related_name='works_authored', on_delete=models.CASCADE)
+    start_date = models.DateField(default=timezone.now)
+    end_date = models.DateField()
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.title
+
+
+class News(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    file = models.FileField(blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
