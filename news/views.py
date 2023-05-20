921ecc50fcb4c8917039b6724ce5f1e41291f1f6
Nikita Pravoi <nikitapravoi@yandex.ru>
2023-05-20 17:02:56 +0700

    Created working <a> tag that redirects you to the card events info

--- news/views.py
+++ news/views.py
@@ -13,24 +13,29 @@ def get_week_cards(request):
     # Создаем карточки на каждый день недели
     for i in range(7):
         card_date = monday + timedelta(days=i)
+
         day_name = calendar.day_name[card_date.weekday()].capitalize()
+
         month_name = calendar.month_name[card_date.month]
 
         events = Event.objects.filter(event_date__date=card_date)
-        event_titles = '\n '.join([event.title for event in events])
+        year = card_date.year.real
+        month = card_date.month.real
+        day = card_date.day.real
+
+        event_titles = [event.title for event in events]
 
         card = {
             'date': f"{day_name}, {card_date.day} {month_name}",
-            'content': event_titles if event_titles else '',
+            'link_date': {'year': year, 'month': month, 'day': day},
+            # 'content': event_titles if event_titles else '',
+            'contents': event_titles if event_titles else ['Нет событий', ],
             'is_current_day': card_date == current_date
         }
         week_cards.append(card)
 
-    today_events = Event.objects.filter(event_date__date=current_date)
-
     context = {
         'week_cards': week_cards,
-        'today_events': today_events,
     }
     return render(request, 'news/main.html', context)
 
@@ -39,11 +44,41 @@ def get_events_by_date(request, year, month, day):
     selected_date = date(year, month, day)
     events = Event.objects.filter(event_date__date=selected_date)
 
+    current_date = date.today()
+    monday = current_date - timedelta(days=current_date.weekday())  # Находим понедельник текущей недели
+
+    week_cards = []
+
+    # Создаем карточки на каждый день недели
+    for i in range(7):
+        card_date = monday + timedelta(days=i)
+
+        day_name = calendar.day_name[card_date.weekday()].capitalize()
+
+        month_name = calendar.month_name[card_date.month]
+
+        events = Event.objects.filter(event_date__date=card_date)
+        year = card_date.year.real
+        month = card_date.month.real
+        day = card_date.day.real
+
+        event_titles = [event.title for event in events]
+
+        card = {
+            'date': f"{day_name}, {card_date.day} {month_name}",
+            'link_date': {'year': year, 'month': month, 'day': day},
+            # 'content': event_titles if event_titles else '',
+            'contents': event_titles if event_titles else ['Нет событий', ],
+            'is_current_day': card_date == current_date
+        }
+        week_cards.append(card)
+
+    today_events = Event.objects.filter(event_date__date=selected_date)
     context = {
-        'selected_date': selected_date,
-        'events': events,
+        'week_cards': week_cards,
+        'today_events': today_events,
     }
-    return render(request, 'news/events.html', context)
+    return render(request, 'news/main.html', context)
 
 
 def get_news_page(request):
